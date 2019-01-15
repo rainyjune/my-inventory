b8da599415d351c9b0a80c4e5a7aa4fe4ca15f9a
rainyjune <rainyjune@live.cn>
2019-01-15 14:58:28 +0800

    Use combineReducers() function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,8 @@
+import { combineReducers } from 'redux';
 import appError from './appError';
 import items from './items';
 import formMode from './formMode';
 import selectedItem from './selectedItem';
 
-const reducer = (state = {
-  items: [],
-  selectedItem: null,
-  formMode: 'NONE',
-  appError: ''
-}, action) => {
-  return {
-    appError: appError(state.appError, action),
-    items: items(state.items, action),
-    formMode: formMode(state.formMode, action),
-    selectedItem: selectedItem(state.selectedItem, action)
-  };
-};
-
+const reducer = combineReducers({appError, items, formMode, selectedItem});
 export default reducer;
